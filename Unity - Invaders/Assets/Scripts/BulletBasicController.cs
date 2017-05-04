df904a073136c360cd0fef034fb15657dbc06870
Luke Savage <lsava22@hotmail.com>
2017-05-04 14:16:47 +1000

    Created Player and Basic Bullet
    -Added Player controls for movement and firing bullets
    -Added Bullet movement code

---
+++ Unity - Invaders/Assets/Scripts/BulletBasicController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBasicController : MonoBehaviour
+{
+    private Rigidbody2D _rb;
+
+    private int _damage = 5;
+    private Vector2 _moveDirection;
+    private float _moveSpeed;
+
+    public int Damage { get { return _damage; } }
+    public Vector2 MoveDirection { get { return _moveDirection; } }
+    public float MoveSpeed { get { return _moveSpeed; } }
+
+	void Start ()
+    {
+        _rb = gameObject.AddComponent<Rigidbody2D>();
+        _rb.freezeRotation = true;
+        _rb.gravityScale = 0;
+        _rb.drag = 1;
+	}
+	
+	void FixedUpdate ()
+    {
+        _rb.AddForce(_moveDirection * _moveSpeed);
+    }
+
+    public void SetBulletParams(Vector2 directionVector, float speed)
+    {
+        _moveDirection = directionVector;
+        _moveSpeed = speed;
+    }
+}
---
+++ Unity - Invaders/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject BulletPrefab;
+    private Rigidbody2D _rb;
+
+    private float _moveSpeed = 30f;
+    private float _shootTimer;
+    private float _shootCooldown = 0.2f;
+
+    public float MoveSpeed { get { return _moveSpeed; } }
+
+	void Start ()
+    {
+        _rb = gameObject.AddComponent<Rigidbody2D>();
+        _rb.freezeRotation = true;
+        _rb.gravityScale = 0;
+        _rb.drag = 5;
+	}
+	
+    void Update()
+    {
+        if(_shootTimer <= 0 && Input.GetKey(KeyCode.Space))
+        {
+            GameObject bullet = Instantiate(BulletPrefab, transform, false);
+            bullet.GetComponent<BulletBasicController>().SetBulletParams(Vector2.up, 50);
+            _shootTimer = _shootCooldown;
+        }
+
+        if (_shootTimer > 0)
+        {
+            _shootTimer -= Time.deltaTime;
+        }
+    }
+
+	void FixedUpdate ()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+        _rb.AddForce(movement * _moveSpeed);
+	}
+}
